543640c31b1f0b970a874943a3c055cd7980fcc2
Wolltoffel <lzandianpour@stud.macromedia.de>
2023-06-05 23:48:59 +0200

    Revert "Merge branch 'main' of https://github.com/Wolltoffel/TabletInterface"
    This reverts commit b500e6e5e77c507c2396729e2a4e15c931379389, reversing
    changes made to 95e7d55885698ca55f871fe6f34eb0756de74076.

---
+++ Tablet_UnityProject/Assets/Scripts/AnalyzerActions/Animations/AnimationManagers/AnimationManagerInstances/MainCanvasAnimations.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+
+public class MainCanvasAnimations: AnimationPlayer
+{
+    [TextArea]
+    [SerializeField] string[] infoTexts;
+    [TextArea]
+    [SerializeField] string[] headerTexts;
+    [TextArea]
+    [SerializeField] string[] codeTexts;
+    
+    [Space(20)]
+    [TextArea]
+    [SerializeField]string[] reportTexts = new string[2];
+    [TextArea]
+    [SerializeField] string[] reportTextHeader = new string[2];
+    
+    [Space(40)]
+    [SerializeField] TextMeshProUGUI headerTextComponent;
+    [SerializeField]TextMeshProUGUI infoTextComponent;
+    [SerializeField] TextMeshProUGUI codeTextComponent;
+    [SerializeField] TextMeshProUGUI reportTextComponent;
+    [SerializeField] TextMeshProUGUI reportTextHeaderComponent;
+    bool fadedIn;
+    bool firstFadeIn = true;
+
+    public override void PlayAnimation(int index, bool firstButtonPress)
+    {
+        int activePresetIndex = GameManager.GiveActivePresetIndex();
+        index = activePresetIndex * 3 + index;
+        reportTextComponent.text = reportTexts[activePresetIndex];
+        reportTextHeaderComponent.text = reportTextHeader[activePresetIndex];
+
+
+
+        if (index > 0)
+        {
+            infoTextComponent.text = infoTexts[index - 1];
+            headerTextComponent.text = headerTexts[index - 1];
+            codeTextComponent.text = codeTexts[index - 1];
+        }
+
+        if (firstFadeIn)
+        {
+            animator.Play("SetUpMainCanvas");
+            firstFadeIn = false;
+            return;
+        }
+
+        if (fadedIn)
+        {
+            animator.Play("FadeOut");
+            fadedIn = false;
+        }
+        else
+        {
+            fadedIn = true;
+            animator.Play("FadeIn");
+        }
+    }
+
+    public override void SetIndex(int newIndex) { }
+    public override void PlayAnimation(int estimatedCost) { }
+    public override int GetIndex()
+    {
+        return 0;
+    }
+
+    public override void  ResetStatus()
+    {
+        fadedIn = false;
+        firstFadeIn = true;
+    }
+}
